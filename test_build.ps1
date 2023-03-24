37ea364fcbba8ecfe5f136dcb450e34ab0fe3403
Michael Ciccotti <mciccotti@divisionsinc.com>
2023-03-24 12:25:17 -0500

    always include dependencies for generated code

--- test_build.ps1
+++ test_build.ps1
@@ -1,3 +1,6 @@
 dotnet restore Grpc.sln --packages=packages
 
 dotnet pack --configuration Release Grpc.Tools --output artifacts
+
+rm artifacts/FsGrpc.Tools.1.0.0.zip
+cp artifacts/FsGrpc.Tools.1.0.0.nupkg artifacts/FsGrpc.Tools.1.0.0.zip
